12d3e936a8f5d2e04c5dd13f0fa80f2245f3dc78
Faizan Zahid <faizan.zahid88@gmail.com>
2023-03-26 20:03:59 +0500

    container page design

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,34 +1,46 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import DropDownWithAccordian from './DropDownWithAccordian'
-import SearchField from './SearchField'
-import TableView from './TableView'
-import BoxesLayout from './BoxesLayout'
-import SubHeader from '../components/SubHeader'
+import * as React from "react";
+import { Grid, Box, Container } from "@mui/material";
+import DropDownWithAccordian from "./DropDownWithAccordian";
+import SearchField from "./SearchField";
+import TableView from "./TableView";
+import BoxesLayout from "./BoxesLayout";
+import SubHeader from "../components/SubHeader";
 
-export default function Container() {
+export default function ContainerWrapper() {
   const toggleHandle = (index: number) => {
-    console.log(index, 'index')
-    setShowView(index)
-  }
-  const [showView, setShowView] = React.useState<number>(1)
- 
+    console.log(index, "index");
+    setShowView(index);
+  };
+  const [showView, setShowView] = React.useState<number>(1);
 
   return (
     <Box sx={{ flexGrow: 1, mt: 3 }}>
-      <SubHeader toggleHandle={toggleHandle} showView={showView}/>
-      <Grid container spacing={3}>
-        <Grid item xs={3}>
-            <DropDownWithAccordian/>
+      <SubHeader toggleHandle={toggleHandle} showView={showView} />
+      <Container maxWidth="xl">
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={3}>
+            <DropDownWithAccordian />
+          </Grid>
+          <Grid item xs>
+            <SearchField />
+            {showView === 1 ? <TableView /> : <BoxesLayout />}
+
+            {/* Pagination */}
+            {/* <Pagination
+              sx={{ p: 3, background: "transparent" }}
+              hidePrevButton={page === 1}
+              hideNextButton={page === Math.ceil(rows.length / 10)}
+              boundaryCount={2}
+              defaultPage={0}
+              onChange={handleChangePage}
+              page={page}
+              count={Math.ceil(rows.length / 10)}
+              variant="outlined"
+              shape="rounded"
+            /> */}
+          </Grid>
         </Grid>
-        <Grid item xs>
-          <SearchField />
-          {
-            showView === 1 ? (<TableView />) : (<BoxesLayout/>)
-          }
-        </Grid>
-      </Grid>
+      </Container>
     </Box>
   );
 }
