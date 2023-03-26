063183eb8119d0e0853e346b3883b72b2c92de51
Faizan Zahid <faizan.zahid88@gmail.com>
2023-03-26 19:35:36 +0500

    removed extra routes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,14 @@
-import React, {ReactElement, FC} from "react";
+import React, { ReactElement, FC } from "react";
 import { Box, Divider } from "@mui/material";
-import SubHeader from '../components/SubHeader'
-import Container from '../components/Container'
-  
+import Container from "../components/Container";
+
 const Home: FC<any> = (): ReactElement => {
-    return (
-        <Box sx={{ minWidth: 275, background: '#eef0f4'}}>
-            {/* <SubHeader/> */}
-            <Divider variant="middle" />
-            <Container/>
-        </Box>
-    );
+  return (
+    <Box sx={{ minWidth: 275, background: "#eef0f4" }}>
+      <Divider variant="middle" />
+      <Container />
+    </Box>
+  );
 };
 
 export default Home;
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,5 @@
 // pages
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Products from "./pages/Products";
 
 // other
 import { FC } from "react";
@@ -23,18 +21,4 @@ export const routes: Array<Route> = [
         enabled: true,
         component: Home
     },
-    {
-        key: 'about-route',
-        title: 'About',
-        path: '/about',
-        enabled: true,
-        component: About
-    },
-    {
-        key: 'products-route',
-        title: 'Products',
-        path: '/products',
-        enabled: true,
-        component: Products
-    }
 ]
