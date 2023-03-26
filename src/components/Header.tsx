8652c8e6e4a3c80fd8d1094ea6ded48961d94a22
Faizan Zahid <faizan.zahid88@gmail.com>
2023-03-27 01:03:36 +0500

    header UI fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,31 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import Divider from '@mui/material/Divider'
-import MenuItem from '@mui/material/MenuItem';
-import Stack from '@mui/material/Stack';
+import * as React from "react";
+import {
+  Stack,
+  MenuItem,
+  Divider,
+  Tooltip,
+  Avatar,
+  Container,
+  Menu,
+  Typography,
+  IconButton,
+  Toolbar,
+  Box,
+  AppBar,
+} from "@mui/material";
 
-import { ReactComponent as LogoIcon } from '../assets/logo.svg';
-import { ReactComponent as CartIcon } from '../assets/cart.svg';
+import { ReactComponent as LogoIcon } from "../assets/logo.svg";
+import { ReactComponent as CartIcon } from "../assets/cart.svg";
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function Header() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
   };
 
   const handleCloseUserMenu = () => {
@@ -44,7 +40,6 @@ function Header() {
             <LogoIcon />
             <Divider orientation="vertical" variant="middle" flexItem />
             <CartIcon />
-
           </Stack>
           <Typography
             variant="h6"
@@ -53,47 +48,67 @@ function Header() {
             href="/"
             sx={{
               mr: 2,
-              padding: '12px 24px 12px 24px',
-              display: { xs: 'none', md: 'flex' },
-              textDecoration: 'none',
-              letterSpacing: 'normal',
-              fontFamily: 'Rubik',
-              color: '#307fc1',
+              padding: "12px 24px 12px 24px",
+              display: { xs: "none", md: "flex" },
+              textDecoration: "none",
+              letterSpacing: "normal",
+              color: "#307fc1",
               lineHeight: 1.2,
-              fontSize: '20px',
+              fontSize: "20px",
               fontWeight: 300,
-              fontStretch: 'normal',
-              fontStyle: 'normal'
-
+              fontStretch: "normal",
+              fontStyle: "normal",
             }}
           >
             IAM SHOP
           </Typography>
-          <Box sx={{ flexGrow: 2, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end' }}>
+          <Box
+            sx={{
+              flexGrow: 2,
+              display: { xs: "none", md: "flex" },
+              justifyContent: "flex-end",
+            }}
+          >
             <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, "&:hover": {backgroundColor: "transparent", } }}>
+              <IconButton
+                onClick={handleOpenUserMenu}
+                sx={{ p: 0, "&:hover": { backgroundColor: "transparent" } }}
+              >
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                <Typography sx={{ p: 2, fontStyle: 'normal', fontStretch: 'normal', letterSpacing: 'normal', color: '#919193', lineHeight: 1.14, fontSize: '14px', fontFamily:'Rubik', fontWeight: 'normal' }}>Patrik Parker</Typography>
+                <Typography
+                  sx={{
+                    p: 2,
+                    fontStyle: "normal",
+                    fontStretch: "normal",
+                    letterSpacing: "normal",
+                    color: "#919193",
+                    lineHeight: 1.14,
+                    fontSize: "14px",
+                    fontWeight: "normal",
+                  }}
+                >
+                  Patrik Parker
+                </Typography>
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+              {settings.map((setting, index) => (
+                <MenuItem key={index} onClick={handleCloseUserMenu}>
                   <Typography textAlign="center">{setting}</Typography>
                 </MenuItem>
               ))}
